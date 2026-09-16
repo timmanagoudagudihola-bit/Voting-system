e6519104c566fa25ac69023b1ae3abd3cf266324
timmanagoudagudihola-bit <timmanagoudagudihola@gmail.com>
2026-09-16 08:51:32 +0530

    Add welcome message to voting eligibility output

--- vote.py
+++ vote.py
@@ -3,6 +3,7 @@ age = int(input("Enter your age: "))
 if age >=18:
   print("You are eligible to vote.")
   print("You can register to vote")
+  print("Welcome to voting system")
 else:
   print("You are not eligible to vote.")
   print("You must atleast 18years old")
